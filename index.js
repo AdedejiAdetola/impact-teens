3749282d0d65a31dcf543f1f6a94983f3b4db2ac
Adetola Adedeji <adetolaadedeji0@gmail.com>
2021-11-05 15:44:54 +0100

    completed nav functionally

--- index.js
+++ index.js
@@ -69,14 +69,24 @@ init();
 
 //toggle
 const menuBtn = document.querySelector(".menu__btn");
+const menuRespond = document.querySelector(".responsive");
+const headText = document.querySelector(".header__text");
 let menuClosed = true;
 
 menuBtn.addEventListener("click", () => {
   if(menuClosed) {
     menuBtn.classList.add("open");
+    headText.style.display="none";
+    menuRespond.classList.add("responsiveBlock");
     menuClosed = false;
   } else {
     menuBtn.classList.remove("open");
-    menuClosed = true;
+    menuClosed = true;   
+    headText.style.display="block"; 
+    menuRespond.classList.remove("responsiveBlock");
   }
 })
+
+
+//toggle dropdown
+//onclick menubtn dropdown
